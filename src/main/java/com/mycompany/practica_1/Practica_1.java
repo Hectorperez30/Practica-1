119c070b756bc59dc39efa738590f997598e0202
Oscar <Oscar@PC>
2024-09-10 22:00:19 -0600

    Carga Inicial

---
+++ src/main/java/com/mycompany/practica_1/Practica_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica_1;
+
+/**
+ *
+ * @author Oscar
+ */
+public class Practica_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
